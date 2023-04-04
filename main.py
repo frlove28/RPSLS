81412a7487bfcd02973658bae17dd26f2ae95215
Zeldalord <frlove@protonmail.com>
2023-04-04 14:43:42 -0700

    blueprint

---
+++ main.py
@@ -0,0 +1,100 @@
+# rules of game
+
+# Best 2 out of 3 wins
+
+# Scissors cuts rock
+# Rock crushes lizard
+# Paper covers rock
+# # lizard poisons spock
+# spock smashes scissors
+# scissors decapitates lizard
+# lizard eats paper
+# paper disproves spock
+# spock vaporizes rock
+# rock crushes scissors
+
+
+
+# Algorithms
+
+# will need user input(): bad user input???
+
+#  As a player, I want the game of RPSLS to be at minimum a 
+# “best of three” to decide a winner. ( while loop, random.choice, if/ else statement )
+
+# show that a player wins each round
+
+
+# enter all hand gestures as a string
+
+
+# determine parent/ child class
+
+# commits throughout 
+
+
+# define rules of game
+
+
+# # make sure we have defined classes
+
+
+# after mvp
+
+
+# create a class for each gesture (separate files)
+
+
+
+
+
+
+
+
+
+
+# Main Stories
+
+
+# (5 points): As a developer, I want to make at least 10 commits with descriptive messages.
+
+
+# (15 points): As a developer, I want to find a way to properly incorporate inheritance into my game.  (define super class)
+
+
+# (5 points): As a developer, I want to account for and handle bad user input, ensuring that any user 
+# input is validated and reobtained if necessary.
+
+
+# (10 points): As a developer, I want to store all of the gesture options/choices in a
+# List (AS STRINGS).
+
+
+# I want to find a way to utilize the list of gestures within my code 
+# (display gesture options, assign player a gesture, etc).
+# NOTE: Do not use a Gesture class until you have reached MVP for all user stories and been checked off by an instructor!
+
+
+# (10 points): As a player, I want the correct player to win a given round based on the choices made by each player. 
+# See below for game rules!
+
+
+# (10 points): As a player, I want the game of RPSLS to be at minimum a “best of three” to decide a winner. 
+
+
+
+# (10 points): As a player, I want the option of a single player (human vs AI) or a multiplayer (human vs human) game.
+
+
+input('Welcome to RPSLS! For One Player. enter "1" for Multiplayer, enter "2"')
+
+
+
+
+
+
+
+
+
+
+
