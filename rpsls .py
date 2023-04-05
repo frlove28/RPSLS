61cf417ba8ce3639ae7e94848c65aba5c2d1f284
Zeldalord <frlove@protonmail.com>
2023-04-05 16:17:57 -0700

    building out user stories

--- rpsls .py
+++ rpsls .py
@@ -1,9 +1,37 @@
 
 
+
 class Rpsls:
     def __init__(self):
         self.player_one
         self.player_two
-
         pass
+
+
+def welcome_message(self):
+    input('Welcome to Rock, Paper, Scissors, Lizard, Spock! To play One Player against Computer enter "1". To play Muliplayer against a friend enter "2"')
+
+print("Rules of Game:") 
+
+print("Best 2 out of 3 wins") 
+print ("Rock crushes lizard")
+print ("Paper covers rock")
+print ("lizard poisons spock")
+print ("spock smashes scissors")
+print ("scissors decapitates lizard")
+print ("lizard eats paper")
+print ("paper disproves spock")
+print ("spock vaporizes rock")
+print ("rock crushes scissors")
+
+user_choice = input(int(1,2))
+pass
+
+def player_one_vs_computer(self):
+    if user_choice == 1:
+        print (input(f"You are playing vs Computer. Round 1. Choose your Weapon: {self.gestures}"))
+
+
+
+
 
