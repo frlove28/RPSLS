457faec90aa36cbe9e1f25821cfb8a7a7f9ce90e
Vince1220 <guitarvince025@yahoo.com>
2023-04-05 15:29:52 -0400

    Add player class

--- player.py
+++ player.py
@@ -1,5 +1,11 @@
 class Player:
-    def __init__(self, player1,player2):
-        self.player1=''
-        self.player2=''
+    def __init__(self, name):
+        self.name = name
+        self.gestures = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        self.score =0
+    
+    def choose_gesture(self):
         pass
+
+    def is_winner(self):
+        return self.score >= 2
